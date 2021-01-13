546ffa0a6aebb4a04392a735c075ee3ee2077684
billyct <billy_allen@126.com>
2021-01-13 20:50:02 +0800

    fix: when export pdf from chrome the skill's background color is tranparent

--- components/Skill.js
+++ components/Skill.js
@@ -1,5 +1,9 @@
+import cn from 'classnames'
+
+import styles from './Skill.module.css'
+
 const Skill = ({children}) => (
-  <span className='py-1 px-2 mr-2 rounded bg-red-100'>
+  <span className={cn('py-1 px-2 mr-2 rounded bg-red-100', styles.skill)}>
     {children}
   </span>
 )
